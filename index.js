2c1f7f6fe1428f4ff72e33bb29f42efafe05e9bd
Justin Chittarath <justinchitt@gmail.com>
2021-09-07 08:44:08 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+victory.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
